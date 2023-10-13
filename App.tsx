034252345c331860f5d11a2b041d80dccef99e72
André Kunde <andrelbkunde@gmail.com>
2023-10-13 07:14:14 -0300

    theme customize

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { View, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { THEME } from './src/theme';
+import { Loading } from '@components/Loading';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
@@ -10,15 +13,13 @@ export default function App() {
   });
 
   return (
-    <NativeBaseProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#323238' }}>
-        <StatusBar
-          barStyle='light-content'
-          backgroundColor='transparent'
-          translucent
-        />
-        {fontsLoaded ? <View />: <View />}
-      </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
