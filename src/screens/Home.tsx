705f60ba04a2d24355cf388bb364d31d3156010d
André Kunde <andrelbkunde@gmail.com>
2023-10-17 10:51:37 -0300

    Group components

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { FlatList, VStack } from 'native-base';
+
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'ombro', 'bíceps', 'tríceps']);
+  const [groupSelected, setGroupSelected] = useState('costas');
+
   return (
     <VStack>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
