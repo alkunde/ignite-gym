592d1836cc99662acf1530978ac93661745f3516
André Kunde <andrelbkunde@gmail.com>
2023-10-25 17:12:42 -0300

    layout end

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,26 @@
 import { useState } from 'react';
-import { FlatList, VStack } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
+import { ExerciseCard } from '@components/ExerciseCard';
 import { HomeHeader } from '@components/HomeHeader';
+
+import { AppNavigatorRouteProps } from '@routes/app.routes';
 
 export function Home() {
   const [groups, setGroups] = useState(['costas', 'ombro', 'bíceps', 'tríceps']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra', 'Polia invertida', 'Trapézio']);
   const [groupSelected, setGroupSelected] = useState('costas');
 
+  const navigation = useNavigation<AppNavigatorRouteProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
+
   return (
-    <VStack>
+    <VStack flex={1}>
       <HomeHeader />
 
       <FlatList
@@ -18,7 +29,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toUpperCase() === item.toUpperCase()}
             onPress={() => setGroupSelected(item)}
           />
         )}
@@ -26,8 +37,34 @@ export function Home() {
         showsHorizontalScrollIndicator={false}
         _contentContainerStyle={{ px: 8 }}
         my={10}
+        minH={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard
+              onPress={handleOpenExerciseDetails}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+
+      </VStack>      
     </VStack>
   );
 }
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,94 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoLoading, setPhotoLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={8}>
+          {photoLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/alkunde.png' }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text color="green.500" fontFamily="heading" fontSize="md" mt={2} mb={6}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input
+            bg="gray.600"
+            fontFamily="body"
+            placeholder="Nome"
+          />
+
+          <Input
+            bg="gray.600"
+            fontFamily="body"
+            value="andrelbkunde@gmail.com"
+            isDisabled
+          />
+        </Center>
+
+        <VStack px={10} mt={6} mb={9}>
+          <Heading color="gray.200" fontSize="md" fontFamily="heading" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input
+            bg="gray.600"
+            fontFamily="body"
+            fontSize="md"
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+
+          <Input
+            bg="gray.600"
+            fontFamily="body"
+            fontSize="md"
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+
+          <Input
+            bg="gray.600"
+            fontFamily="body"
+            fontSize="md"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button
+            title="Atualizar"
+            mt={4}
+          />
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
